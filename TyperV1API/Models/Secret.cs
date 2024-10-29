fbde032c8bf1c29316c078d37fc210538be5d37e
Eli <elireid.28@gmail.com>
2024-10-29 11:04:27 -0400

    test

--- TyperV1API/Models/Secret.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TyperV1API.Models
-{
-    public class Secret
-    {
-        public static string url = "Data Source=.;Initial Catalog=TyperV1; Integrated Security=SSPI;Encrypt=false;TrustServerCertificate=True;";
-    }
-}
--- TyperV1API/Models/TyperV1Context.cs
+++ TyperV1API/Models/TyperV1Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using TypicalTypistAPI;
 
 namespace TyperV1API.Models;
 
